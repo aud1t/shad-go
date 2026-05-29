b318dff613e88ad90d056814b587ce034afa4709
sznarzullozoda <sznarzullozoda@yandex.ru>
2026-05-29 18:15:39 +0500

    wordcount solved

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,32 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	files := os.Args[1:]
+	wordCount := make(map[string]int)
+	for _, file := range files {
+		dat, err := os.ReadFile(file)
+		check(err)
+		words := strings.Split(string(dat), "\n")
+		for _, word := range words {
+			wordCount[word]++
+		}
+	}
+	for word, cnt := range wordCount {
+		if cnt > 1 {
+			fmt.Printf("%d\t%s\n", cnt, word)
+		}
+	}
 }

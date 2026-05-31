5009774147f0c9a9ce0325f5331803f83971b69f
sznarzullozoda <sznarzullozoda@yandex.ru>
2026-05-31 15:34:17 +0500

    hogwarts solved

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -2,6 +2,29 @@
 
 package hogwarts
 
-func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+func GetCourseList(prereqs map[string][]string) (res []string) {
+	learned := make(map[string]int)
+	for course := range prereqs {
+		if v := learned[course]; v == 2 {
+			continue
+		}
+		res = append(res, dfs(course, prereqs, learned)...)
+	}
+	return
+}
+
+func dfs(course string, prereqs map[string][]string, learned map[string]int) (res []string) {
+	learned[course] = 1
+	for _, preqCourse := range prereqs[course] {
+		v, ok := learned[preqCourse]
+		if ok && v == 2 {
+			continue
+		} else if ok && v == 1 {
+			panic("cycle")
+		}
+		res = append(res, dfs(preqCourse, prereqs, learned)...)
+	}
+	res = append(res, course)
+	learned[course] = 2
+	return
 }

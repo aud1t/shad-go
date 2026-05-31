e78900ef7fbc24d9205db44d7c71af82b0024c1d
sznarzullozoda <sznarzullozoda@yandex.ru>
2026-05-31 15:33:49 +0500

    hotelbusiness solved

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "slices"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -12,6 +14,33 @@ type Load struct {
 	GuestCount int
 }
 
-func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+func ComputeLoad(guests []Guest) (result []Load) {
+	if len(guests) == 0 {
+		return
+	}
+
+	events := make(map[int]int)
+	for _, g := range guests {
+		events[g.CheckInDate]++
+		events[g.CheckOutDate]--
+	}
+
+	uniqDays := []int{}
+	for d := range events {
+		uniqDays = append(uniqDays, d)
+	}
+	slices.Sort(uniqDays)
+
+	currentCnt := 0
+	for _, dt := range uniqDays {
+		if events[dt] == 0 {
+			continue
+		}
+		currentCnt += events[dt]
+		result = append(result, Load{
+			StartDate:  dt,
+			GuestCount: currentCnt,
+		})
+	}
+	return
 }

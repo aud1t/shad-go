6385dcf8d9ca760c13ab70f818e38cc7aa784d05
sznarzullozoda <sznarzullozoda@yandex.ru>
2026-06-01 00:23:09 +0500

    utf8/reverse and utf8/spacecollapce solved

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,18 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var sb strings.Builder
+	sb.Grow(len(input))
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		sb.WriteRune(r)
+		input = input[:len(input)-size]
+	}
+	return sb.String()
 }

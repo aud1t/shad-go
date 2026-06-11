1912ff6571f50660a91846b6a4ec2bb96d1856d9
sznarzullozoda <sznarzullozoda@yandex.ru>
2026-06-11 23:01:07 +0500

    once solved better

--- once/once.go
+++ once/once.go
@@ -2,31 +2,20 @@
 
 package once
 
-import (
-	"errors"
-	"fmt"
-)
-
-type PanicInFErr struct {
-	panicMsg any
-}
-
-func (p *PanicInFErr) Error() string {
-	return fmt.Sprint(p.panicMsg)
-}
-
 // Once describes an object that will perform exactly one action.
 type Once struct {
-	blockChan chan struct{}
-	done      chan struct{}
+	call chan struct{}
+	done chan struct{}
 }
 
 // New creates Once.
 func New() *Once {
-	return &Once{
-		blockChan: make(chan struct{}),
-		done:      make(chan struct{}),
+	o := &Once{
+		call: make(chan struct{}, 1),
+		done: make(chan struct{}),
 	}
+	o.call <- struct{}{}
+	return o
 }
 
 // Do calls the function f if and only if Do is being called for the
@@ -46,32 +35,14 @@ func New() *Once {
 // If f panics, Do considers it to have returned; future calls of Do return
 // without calling f.
 func (o *Once) Do(f func()) {
-	defer func() {
-		if r := recover(); r != nil {
-			if err, ok := r.(error); ok {
-				var p *PanicInFErr
-				if errors.As(err, &p) {
-					// паника произошла при вызове f
-					panic(p.panicMsg)
-				}
-				// паника произошла при повторном закрытии канала(повторный вызов Do), ничего не делаем
-			}
-			// если конкуррентно вызвали Do ждём завершения первого f
-			<-o.done
-		}
-	}()
-	close(o.blockChan)
+	select {
+	case <-o.call:
+		// Первыми выполним f()
+	case <-o.done:
+		return
+	}
 
 	defer close(o.done)
-
-	defer func() {
-		if r := recover(); r != nil {
-			p := &PanicInFErr{
-				panicMsg: r,
-			}
-			panic(fmt.Errorf("panic in f(): %w", p))
-		}
-	}()
 
 	f()
 }
